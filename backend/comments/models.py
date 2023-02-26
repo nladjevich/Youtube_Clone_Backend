f27392d3a02cdf5abb726f60d7ca26cf108d9176
Nicholas <zantladjevich@gmail.com>
2023-02-26 14:06:18 -0500

    Created comments application

---
+++ backend/comments/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+class Car(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    make = models.CharField(max_length=30)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
