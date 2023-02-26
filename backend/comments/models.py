504187d6d14b4e348c72846128d57a5e80d86fd5
Nicholas <zantladjevich@gmail.com>
2023-02-26 14:14:59 -0500

    Created Comments model

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from authentication.models import User
 
 # Create your models here.
-class Car(models.Model):
+class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    make = models.CharField(max_length=30)
-    model = models.CharField(max_length=100)
-    year = models.IntegerField()
+    video_id = models.CharField(max_length=30)
+    text = models.CharField(max_length=100)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
